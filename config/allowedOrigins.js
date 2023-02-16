c2c210fac020d567d8c20549e890b641747e6407
Pacifique Kwizera <kwizerapacifique19@gmail.com>
2023-02-16 22:24:32 +0200

    cors configurations

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 export const allowedOrigins = [
-    'https://www.kwizera-117.netlify.app',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://localhost:3000'
+  "https://www.kwizera-117.netlify.app",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+  "http://localhost:3000",
+  "*",
 ];
