3ac59dcccf0714e11b09f09d60130f5ef729580c
Pacifique Kwizera <kwizerapacifique19@gmail.com>
2023-02-21 16:07:49 +0200

    cyclic deployment

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,3 +1,7 @@
 module.exports = {
   presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+  plugins: [
+    "@babel/plugin-proposal-optional-chaining",
+    "@babel/plugin-proposal-nullish-coalescing-operator",
+  ],
 };
